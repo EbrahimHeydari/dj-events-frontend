92d35fd7c316b7e3329f6821d057b894bd677e05
Ebrahim <akbrly42@gmail.com>
2022-08-06 18:26:56 +0430

    6. Create Custome 404 page

---
+++ pages/404.js
@@ -0,0 +1,20 @@
+import Link from "next/link"
+import Layout from "../components/layout"
+import styles from '../styles/404.module.css'
+import Image from 'next/image'
+
+const NotFoundPage = () => {
+  return (
+    <Layout title='Page Not Found!'>
+      <div className={styles.error}>
+        <h1>
+          <Image src='/images/exclamation.svg' width={40} height={40} /> 404
+        </h1>
+        <h4>Sorry, There is nothing here</h4>
+        <Link href='/'>Go Back Home</Link>
+      </div>
+    </Layout>
+  )
+}
+
+export default NotFoundPage
