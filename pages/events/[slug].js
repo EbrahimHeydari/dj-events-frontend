62968a6031b71dd6506464f2379f1e8ba084f0c9
Ebrahim <akbrly42@gmail.com>
2022-08-06 17:09:46 +0430

    3. Next Link Router

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,7 +1,12 @@
+import { useRouter } from 'next/router'
+
 const EventPage = () => {
+  const router = useRouter()
   return (
     <div>
-
+      <h1>Events</h1>
+      <h3>{router.query.slug}</h3>
+      <button onClick={() => router.push('/')}>Go back</button>
     </div>
   )
 }
