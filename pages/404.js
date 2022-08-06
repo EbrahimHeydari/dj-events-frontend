ee6781dff0bf9486ce5462e8b44a44fcb56ca637
Ebrahim <akbrly42@gmail.com>
2022-08-06 18:38:50 +0430

    7. module aliases with jsconfig

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,6 @@
 import Link from "next/link"
-import Layout from "../components/layout"
-import styles from '../styles/404.module.css'
+import Layout from "@/components/layout"
+import styles from '@/styles/404.module.css'
 import Image from 'next/image'
 
 const NotFoundPage = () => {
