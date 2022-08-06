827038916a38dd7b5bfa22ccc3991c099183d833
Ebrahim <akbrly42@gmail.com>
2022-08-06 12:45:19 +0430

    2.pages and routing

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>This is an app to find the lates dj and other musical events</p>
+      <p>Version: 1.0.0</p>
+    </div>
+  )
+}
+
+export default AboutPage
