b2836d3baf629f164f83723ee40b08ba4d1aa76c
Ebrahim <akbrly42@gmail.com>
2022-08-14 16:44:46 +0430

    7-4. Login _ Get JWT

--- pages/account/login.js
+++ pages/account/login.js
@@ -1,17 +1,20 @@
 import 'react-toastify/dist/ReactToastify.css'
 import styles from '@/styles/AuthForm.module.css'
 import Layout from '@/components/Layout'
-import { useContext, useState } from 'react'
-import Image from 'next/image'
-import { ToastContainer } from 'react-toastify'
-import Link from 'next/link'
+import { useContext, useEffect, useState } from 'react'
+import { toast, ToastContainer } from 'react-toastify'
 import AuthContext from '@/context/AuthContext'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const LoginPage = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-
   const { login, error } = useContext(AuthContext)
+
+  useEffect(() => {
+    error && toast.error(error)
+  })
 
   const handleSubmit = e => {
     e.preventDefault()
@@ -20,12 +23,12 @@ const LoginPage = () => {
 
   return (
     <Layout title='User Login'>
+      <ToastContainer theme='colored' />
       <div className={styles.auth}>
         <h1>
           <Image src='/images/icon/user.png' height={32} width={32} />
           <span>Log In</span>
         </h1>
-        <ToastContainer />
         <form onSubmit={handleSubmit}>
           <div>
             <label htmlFor="email">Email Address</label>
